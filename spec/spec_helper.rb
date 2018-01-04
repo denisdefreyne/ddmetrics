05f1b3a9fdd5901920b3bf226dd72776fe87265f
Denis Defreyne <denis.defreyne@stoneship.org>
2018-01-04 21:41:43 +0100

    Fix Rubocop offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,6 @@ require 'timecop'
 
 RSpec.configure do |c|
   c.fuubar_progress_bar_options = {
-    format: '%c/%C |<%b>%i| %p%%', # rubocop:disable Style/FormatStringToken
+    format: '%c/%C |<%b>%i| %p%%',
   }
 end
